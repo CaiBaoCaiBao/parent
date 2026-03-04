ce17faa15d0277a9844633ec8d2b0f047c9a4785
CaiBaoCaiBao <13503089560@163.com>
2026-03-04 21:18:21 +0800

    创建通用模块和用户模块

---
+++ common/src/main/java/common/CommonApplication.java
@@ -0,0 +1,16 @@
+package common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("common")
+public class CommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class, args);
+    }
+
+}
