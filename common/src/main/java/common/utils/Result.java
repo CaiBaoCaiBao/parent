75245635f0d69c236721026dbea72fe19bb46fb9
CaiBaoCaiBao <13503089560@163.com>
2026-03-04 22:40:43 +0800

    通用模块-更新统一响应格式

--- common/src/main/java/common/utils/Result.java
+++ common/src/main/java/common/utils/Result.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -16,30 +17,32 @@ public class Result<T> implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Integer code;
+    private Boolean success;
     private String message;
     private T data;
+    private LocalDateTime timestamp;
 
     public static <T> Result<T> success() {
-        return new Result<>(200, "操作成功", null);
+        return new Result<>(200,true, "操作成功", null,LocalDateTime.now());
     }
 
     public static <T> Result<T> success(T data) {
-        return new Result<>(200, "操作成功", data);
+        return new Result<>(200, true,"操作成功", data,LocalDateTime.now());
     }
 
     public static <T> Result<T> success(String message, T data) {
-        return new Result<>(200, message, data);
+        return new Result<>(200,true, message, data,LocalDateTime.now());
     }
 
     public static <T> Result<T> error() {
-        return new Result<>(500, "操作失败", null);
+        return new Result<>(500,false, "操作失败", null,LocalDateTime.now());
     }
 
     public static <T> Result<T> error(String message) {
-        return new Result<>(500, message, null);
+        return new Result<>(500,false, message, null,LocalDateTime.now());
     }
 
     public static <T> Result<T> error(Integer code, String message) {
-        return new Result<>(code, message, null);
+        return new Result<>(code,false, message, null,LocalDateTime.now());
     }
 }
